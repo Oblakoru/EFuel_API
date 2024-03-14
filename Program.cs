5aab40d925522c69b51afa5bd17d6265db3ac613
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2024-03-14 16:24:21 +0100

    povezava na mongodb atlas server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+
 namespace EFuel_API
 {
+
+    
+
     public partial class Program
     {
         public static void Main(string[] args)
@@ -23,6 +30,26 @@ namespace EFuel_API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            const string connectionUri = "mongodb+srv://david:cLBsxBvxa6LZirHw@cluster0.afizd2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+            // Set the ServerApi field of the settings object to set the version of the Stable API on the client
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+            // Create a new client and connect to the server
+            var client = new MongoClient(settings);
+
+            // Send a ping to confirm a successful connection
+            try
+            {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
 
             Novice(app);
