c6526c51a9a30a417664dccb62e601fe7697ec21
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2024-03-15 18:15:08 +0100

    delujoc get klic - primer

--- Program.Novice.cs
+++ Program.Novice.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace EFuel_API
@@ -8,45 +9,23 @@ namespace EFuel_API
         public static void Novice(WebApplication app, MongoClient client)
         {
 
- 
             var databaseName = "eFuel";
             var collectionName = "Ponudba_goriva";
 
-            var database = client.GetDatabase(databaseName);
-            var collection = database.GetCollection<BsonDocument>(collectionName);
-
-            var filter = new BsonDocument();
-            var documents = collection.Find(filter).ToList();
-
-            Console.WriteLine($"Retrieved {documents.Count} documents from the collection '{collectionName}':");
-            foreach (var document in documents)
+            app.MapGet("/pridobiPonudbo", async () =>
             {
-                Console.WriteLine(document.ToJson());
-            }
 
-            app.MapGet("/pozdrav", () => Results.Ok("hey"));
-            app.MapGet("/test", () => Results.Ok("test"));
+                var database = client.GetDatabase(databaseName);
+                var collection = database.GetCollection<BsonDocument>(collectionName);
 
-           // app.MapGet("/documents", async (IMongoClient client) =>
-           // {
-           //     try
-           //     {
-           //         // Get reference to the MongoDB collection
-           //         var database = client.GetDatabase(databaseName);
-           //         var collection = database.GetCollection<BsonDocument>(collectionName);
-           //
-           //         // Perform the basic GET query to retrieve all documents from the collection
-           //         var filter = new BsonDocument();
-           //         var documents = await collection.Find(filter).ToListAsync();
-           //
-           //         // Serialize documents to JSON and return as response
-           //         return Results.Ok(documents);
-           //     }
-           //     catch (Exception ex)
-           //     {
-           //         return Results.BadRequest($"Error retrieving documents: {ex.Message}");
-           //     }
-           // });
+                var result = await collection.Find(_ => true).ToListAsync();
+
+                var jsonSerializer = new MongoDB.Bson.IO.JsonWriterSettings { OutputMode = MongoDB.Bson.IO.JsonOutputMode.Strict };
+                var json = result.ToJson(jsonSerializer);
+
+                return Results.Ok(json);
+            });
+
 
         }
     }
