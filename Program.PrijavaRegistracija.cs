508e0cb855f15d704fdc7cd675d329db0ff168c0
Miki071 <mirnes.mujabasic@student.um.si>
2024-03-16 00:31:46 +0100

    Dodan model uporabnik, in narejeta metoda za registracijo

--- Program.PrijavaRegistracija.cs
+++ Program.PrijavaRegistracija.cs
@@ -1,4 +1,7 @@
-﻿using MongoDB.Driver;
+﻿using EFuel_API.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.RegularExpressions;
 
 namespace EFuel_API
 {
@@ -6,8 +9,47 @@ namespace EFuel_API
     {
         public static void PrijavaRegistracija(WebApplication app, MongoClient client)
         {
+            var name = "eFuel";
+            var collectionName = "Uporabniki";
+
+            var db = client.GetDatabase("name");
+            var collection = db.GetCollection<BsonDocument>(collectionName);
+
+
             app.MapGet("/login", () => Results.Ok("login"));
-            app.MapGet("/register", () => Results.Ok("register"));
+
+
+            app.MapGet("/register", async (Uporabnik uporabnik) => {
+
+                if (uporabnik != null)
+                {
+                    if (string.IsNullOrEmpty(uporabnik.Ime) || string.IsNullOrEmpty(uporabnik.Priimek) || string.IsNullOrEmpty(uporabnik.E_posta) || string.IsNullOrEmpty(uporabnik.Geslo) )
+                    {
+                        return Results.BadRequest("Niso podani vsi zahtevani podatki!");
+                    }
+
+                    if (!Regex.IsMatch(uporabnik.E_posta, @"@.*\.com"))
+                    {
+                        return Results.BadRequest("Napačno podana epošta!");
+                    }
+
+                    var userObstaja = await collection.Find(Builders<BsonDocument>.Filter.Eq("E_posta", uporabnik.E_posta)).AnyAsync();
+
+                    if (userObstaja)
+                    {
+                        return Results.BadRequest("Epošta več obstaja!");
+                    }
+
+                    await collection.InsertOneAsync(uporabnik.ToBsonDocument());
+
+                    return Results.Ok("Uporabnik je registriran");
+                }
+                else 
+                {
+                    return Results.BadRequest("Napačno podani podatki!");
+                }
+            
+            });
         }
     }
 }
