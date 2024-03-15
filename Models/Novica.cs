22c8b26afc3c4825861f364db4e7e6fc4f54a62c
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2024-03-15 18:49:38 +0100

    dodane osnovne crud operacije novicam

---
+++ Models/Novica.cs
@@ -0,0 +1,11 @@
+﻿namespace EFuel_API.Models
+{
+  
+    public class NovicaModel
+    {
+        public string Id { get; set; }
+        public string Naslov { get; set; }
+        public string Novica { get; set; }
+        public DateTime DatumObjave { get; set; }
+    }
+}
--- Program.Novice.cs
+++ Program.Novice.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFuel_API.Models;
+using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -10,9 +11,9 @@ namespace EFuel_API
         {
 
             var databaseName = "eFuel";
-            var collectionName = "Ponudba_goriva";
+            var collectionName = "Novice";
 
-            app.MapGet("/pridobiPonudbo", async () =>
+            app.MapGet("/pridobiNovice", async () =>
             {
 
                 var database = client.GetDatabase(databaseName);
@@ -24,6 +25,56 @@ namespace EFuel_API
                 var json = result.ToJson(jsonSerializer);
 
                 return Results.Ok(json);
+            });
+
+            app.MapGet("/pridobiNovice/{date}", async (string date) =>
+            {
+                DateTime specifiedDate;
+                if (!DateTime.TryParse(date, out specifiedDate))
+                {
+                    return Results.BadRequest("Invalid date format. Please provide the date in yyyy-MM-dd format.");
+                }
+
+                var database = client.GetDatabase(databaseName);
+                var collection = database.GetCollection<BsonDocument>(collectionName);
+
+                var filter = Builders<BsonDocument>.Filter.Eq("datum_objave", specifiedDate.ToString("yyyy-MM-dd"));
+                var result = await collection.Find(filter).ToListAsync();
+
+                var jsonSerializer = new MongoDB.Bson.IO.JsonWriterSettings { OutputMode = MongoDB.Bson.IO.JsonOutputMode.JavaScript };
+                var json = result.ToJson(jsonSerializer);
+
+                return Results.Ok(json);
+            });
+
+            app.MapGet("/pridobiNovice/{fromDate}/{toDate}", async (DateTime fromDate, DateTime toDate) =>
+            {
+                var database = client.GetDatabase(databaseName);
+                var collection = database.GetCollection<BsonDocument>(collectionName);
+
+                var filter = Builders<BsonDocument>.Filter.And(
+                    Builders<BsonDocument>.Filter.Gte("datum_objave", fromDate.ToString("yyyy-MM-dd")),
+                    Builders<BsonDocument>.Filter.Lte("datum_objave", toDate.ToString("yyyy-MM-dd"))
+                );
+
+                var result = await collection.Find(filter).ToListAsync();
+
+                var jsonSerializer = new MongoDB.Bson.IO.JsonWriterSettings { OutputMode = MongoDB.Bson.IO.JsonOutputMode.JavaScript };
+                var json = result.ToJson(jsonSerializer);
+
+                return Results.Ok(json);
+            });
+
+            app.MapDelete("/izbrisiNovico/{id}", async (string id) =>
+            {
+                var database = client.GetDatabase(databaseName);
+                var collection = database.GetCollection<BsonDocument>(collectionName);
+
+                var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+
+                var result = await collection.DeleteOneAsync(filter);
+
+                return Results.NoContent();
             });
 
 
