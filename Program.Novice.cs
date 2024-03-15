41026c2968758ea553a438b1c385278587614050
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2024-03-15 20:53:24 +0100

    dodal sem funkcionalnost za dodajanje nove novice kjer lahko uporabnik doda svojo novico in pridobivanje novice iz baze po Id novice

--- Program.Novice.cs
+++ Program.Novice.cs
@@ -82,6 +82,82 @@ namespace EFuel_API
                 return Results.Ok(json);
             });
 
+            /// <summary>
+            /// Endpoint za iskanje novice o prometu po njenem ID.
+            /// </summary>
+            /// <param name="id">ID prometne novice, ki jo želite pridobiti.</param>
+            /// <returns>Vrne prometne novice z navedenim ID, če so bile najdene, ali ustrezen odgovor o napaki v nasprotnem primeru..</returns>
+
+            app.MapGet("/pridobiNovico/{id}", async (string id) =>
+            {
+                try
+                {
+                    var objectId = ObjectId.Parse(id);
+
+                    var filter = Builders<BsonDocument>.Filter.Eq("_id", objectId);
+
+                    var result = await collection.Find(filter).FirstOrDefaultAsync();
+
+                    if (result != null)
+                    {
+                        var dict = result.ToDictionary();
+                        return Results.Ok(dict);
+                    }
+                    else
+                    {
+                        return Results.NotFound($"Prometna novia z ID: '{id}' ni bil najden!.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    return Results.BadRequest("Nepravilna oblika ID. Navedite veljavni ObjectId..");
+                }
+                catch (Exception ex)
+                {
+                    return Results.BadRequest($"Napaka: {ex.Message}");
+                }
+            });
+
+
+            /// <summary>
+            /// Endpoint za dodajanje novih prometnih novic.
+            /// </summary>
+            /// <param name="novica">Novica o prometu, ki jo je treba dodati.</param>
+            /// <returns>Vrne odgovor, ki označuje stanje operacije.</returns>
+
+            app.MapPost("/dodajNovico", async (NovicaModel novica) =>
+            {
+                try
+                {
+                    var novicaDocument = new BsonDocument
+                        {
+                        { "naslov", novica.Naslov },
+                        { "novica", novica.Novica },
+                        { "datum_objave", novica.DatumObjave }
+                    };
+                   
+
+                    await collection.InsertOneAsync(novicaDocument);
+
+                    var insertedId = novicaDocument["_id"].AsObjectId.ToString();
+
+                    var insertedNovica = new NovicaModel
+                    {
+                        Id = insertedId,
+                        Naslov = novica.Naslov,
+                        Novica = novica.Novica,
+                        DatumObjave = novica.DatumObjave
+                    };
+
+                    return Results.Created($"/dodajNovico/{insertedId}", insertedNovica);
+                }
+                catch (Exception ex)
+                {
+                    return Results.BadRequest($"Napaka: {ex.Message}");
+                }
+            });
+
+
             app.MapDelete("/izbrisiNovico/{id}", async (string id) =>
             {
 
