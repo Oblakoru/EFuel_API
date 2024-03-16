a3e54ad550868bef4980b68846374df98cc59edd
Miki071 <mirnes.mujabasic@student.um.si>
2024-03-16 20:08:40 +0100

    Dodana metoda GPS

--- Program.GPS.cs
+++ Program.GPS.cs
@@ -1,4 +1,7 @@
-﻿using MongoDB.Driver;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using EFuel_API.Models;
 
 namespace EFuel_API
 {
@@ -6,7 +9,22 @@ namespace EFuel_API
     {
         public static void GPS(WebApplication app, MongoClient client)
         {
-            app.MapGet("/gps", () => Results.Ok("gps"));
+            /*"London" 51.5074, -0.1278
+             "Los Angeles" 34.0522, -118.2437
+             "New York" 40.7128, -74.0060
+             */
+
+            app.MapPost("/gps", ([FromBody] LokacijaUporabnika lokacijaUporabnika) => {
+
+                if(string.IsNullOrEmpty(lokacijaUporabnika.ZemljepisnaDolzina.ToString()) || string.IsNullOrEmpty(lokacijaUporabnika.ZemljepisnaSirina.ToString()))
+                {
+                    return Results.BadRequest("Napačno podana lokacija!");
+                }
+
+                var googleMaps = $"https://www.google.com/maps/place/{lokacijaUporabnika.ZemljepisnaSirina},{lokacijaUporabnika.ZemljepisnaDolzina}";
+
+                return Results.Ok(googleMaps);
+            });
         }
     }
 }
